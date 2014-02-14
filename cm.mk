14a25e4392dc4bed3882fa2de1fda349e9eb6d99
Arne Coucheron <arco68@gmail.com>
2014-02-14 02:17:38 +0100

    meliusltexx: Change inherit order
     * Move comon_full_phone.mk to top. Otherwise default notification
       and alarm tone will be completely wrong
    
    Change-Id: Ie75654989ff27b0ea639157fd79c3f5e18355919
    
    Conflicts:
    	cm.mk

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=meliusltexx TARGET_DEVICE=meliuslte BUILD_FINGERPRINT="samsung/meliusltexx/meliuslte:4.2.2/JDQ39/I9205XXUAMEA:user/release-keys" PRIVATE_BUILD_DESC="meliusltexx-user 4.2.2 JDQ39 I9205XXUAMEA release-keys"
 
