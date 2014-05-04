a8eddc54e71833f02c8d9f9c61c7138bf0395114
Silesh K Nair <sileshn@gmail.com>
2014-05-04 19:58:18 +0530

    Remove unwanted files

--- gzr.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/gzr/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/gzr/config/common_full_phone.mk)
-
-PRODUCT_NAME := gzr_meliusltexx
