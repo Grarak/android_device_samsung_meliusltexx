dcff77ee9967f1daa5a92615f6faee4b67c5a583
Silesh K Nair <sileshn@gmail.com>
2014-05-10 19:50:18 +0530

    Create crom.mk

---
+++ crom.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/samsung/meliusltexx/full_meliultexx.mk)
+
+# Inherit some common C-RoM stuff.
+$(call inherit-product, vendor/crom/config/common_phone.mk)
+
+PRODUCT_DEVICE := meliuslte
+
+PRODUCT_NAME := crom_meliuslte
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/media/xhdpi/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
