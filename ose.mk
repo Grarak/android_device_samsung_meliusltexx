1d573c8f326deb29cb916ceb6117036a6e83117f
Silesh K Nair <sileshn@gmail.com>
2014-05-10 19:50:40 +0530

    Delete ose.mk

--- ose.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
-
-# Inherit some common OSE stuff.
-$(call inherit-product, vendor/ose/config/common_full_phone.mk)
-
-PRODUCT_NAME := ose_meliusltexx
