dd0e8acef2466e651a35162fc403bb87c5b88abe
Willi Ye <williye97@gmail.com>
2014-05-13 15:25:47 +0200

    remove blobs and add carrier
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,14 +20,11 @@
 # definition file).
 #
 
-# Inherit from common serrano
+# Inherit from common melius
 -include device/samsung/melius-common/BoardConfigCommon.mk
 
-# Inherit from the proprietary version
--include vendor/samsung/meliusltexx/BoardConfigVendor.mk
-
 # Assert
-TARGET_OTA_ASSERT_DEVICE := meliuslte,meliusltexx,i9205,GT-I9205
+TARGET_OTA_ASSERT_DEVICE := meliuslte,meliusltexx
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8930_melius_eur_lte_defconfig
--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/meliusltexx/meliusltexx-vendor.mk)
 
 # Inherit from melius-common
 $(call inherit-product, device/samsung/melius-common/melius-common.mk)
@@ -26,3 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/meliusltexx/overlay
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/init.carrier.rc:root/init.carrier.rc
