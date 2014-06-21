0926d349188cde03a82c98372d96b0221baf394b
Silesh K Nair <sileshn@gmail.com>
2014-06-21 08:36:07 +0530

    Remove unwanted make and dependency files
    Moved to their respective branches

--- tg.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
-
-PRODUCT_NAME := tg_meliusltexx
